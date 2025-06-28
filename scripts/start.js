0f49afb513e54f4fcd471ca1ebc720c8fcb169c2
usernameman12 <devorionv2@gmail.com>
2025-06-28 14:35:36 -0700

    add uv static to public and root

--- scripts/start.js
+++ scripts/start.js
@@ -10,6 +10,10 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('proxy', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static', 'iframe.html'));
+}); //usage : /proxy#https://linkhere.com
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
