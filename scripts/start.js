9db9ead3dc20bdcf447bdf860023c929ccdbb2d4
usernameman12 <devorionv2@gmail.com>
2025-06-28 14:59:19 -0700

    fix start script for commonJS

--- scripts/start.js
+++ scripts/start.js
@@ -1,18 +1,22 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Proxy route (example usage: /proxy#https://example.com)
+app.get('/proxy', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static', 'iframe.html'));
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-app.get('proxy', (req, res) => {
-  res.sendFile(path.join(__dirname, 'static', 'iframe.html'));
-}); //usage : /proxy#https://linkhere.com
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
